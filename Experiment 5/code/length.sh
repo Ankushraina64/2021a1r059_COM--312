35dde3766093f750e592363f4fdca8a6959038d9
Ankushraina64 <113122150+Ankushraina64@users.noreply.github.com>
2022-12-17 18:51:50 +0530

    Add files via upload

---
+++ Experiment 5/code/length.sh
@@ -0,0 +1,2 @@
+str ="Welcome to OS lab"
+echo "The total number of char in the string are : ${#str}"
---
+++ Experiment 5/code/string_length.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  char a[100]="Welcome to Operating System Lab";
+  int length;
+
+  length = strlen(a);
+
+  printf("Length of the string = %d\n", length);
+
+  return 0;
+}
