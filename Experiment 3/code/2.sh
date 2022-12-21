fdddb4ee0c94bf815aae57c060ff4e29c88f3e1a
Ankushraina64 <113122150+Ankushraina64@users.noreply.github.com>
2022-12-21 22:15:17 +0530

    Add files via upload

---
+++ Experiment 3/code/2.sh
@@ -0,0 +1,11 @@
+echo "Enter File"
+read str 
+if test -f $str
+then echo "File Exists and is an Ordinary File"
+elif test -d $str
+then echo "It is a Directory File"
+else
+echo "File doesn't exists"
+fi
+Footer
+© 2022 GitHub,
